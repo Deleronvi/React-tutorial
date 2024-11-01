4de4b0c8aeee5ce98e4afd0ace3d209f86e4a1d3
Deleronvi <byronochieng467@gmail.com>
2024-11-01 15:04:08 +0300

    react list

--- src/index.js
+++ src/index.js
@@ -3,27 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-function MissedGoal() {
-	return <h1>MISSED!</h1>;
+function Car(props) {
+  return <li>I am a { props.brand }</li>;
 }
 
-function MadeGoal() {
-	return <h1>GOAL!</h1>;
-}
-
-function Goal(props) {
-  const isGoal = props.isGoal;
-  if (isGoal) {
-    return <MadeGoal/>;
-  }
-  return <MissedGoal/>;
+function Garage() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <>
+      <h1>Who lives in my garage?</h1>
+      <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Goal isGoal={true} />);
+root.render(<Garage />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an anastatlytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
